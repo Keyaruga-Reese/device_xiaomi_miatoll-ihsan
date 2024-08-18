e472c5c4e1df02f96ac25645cb7dbd72cec1b70d
johnmart19 <johnivan19999@gmail.com>
2024-08-18 12:04:38 +0800

    miatoll: Import Misys HAL
    - Import Xiaomi Misys hal for camera watermark
    - Build Legacy android.hidl.memory.block, android.hidl.manager in vendor for misys serivce
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.xiaomi.hardware.misys@3.0-service": library "android.hidl.memory.block@1.0.so" not found: needed by /vendor/lib64/vendor.xiaomi.hardware.misys@3.0.so in namespace (default)
    - 636 I hwservicemanager: getTransport: Cannot find entry vendor.xiaomi.hardware.misys@1.0::IMiSys/default in either framework or device VINTF manifest.
    
    Co-authored-by: klozz <klozz@thexperienceproject.org>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,8 @@ VENDOR_SECURITY_PATCH := 2023-05-01
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 -include vendor/xiaomi/miuicamera/SEPolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Shims
