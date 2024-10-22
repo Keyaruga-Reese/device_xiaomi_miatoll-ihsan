fb435e644c5b9ed3512ff997a23989bef109345f
clarencelol <clarencekuiek@proton.me>
2024-10-22 12:14:49 +0800

    miatoll: Remove additional MiuiCamera sepolicy.mk
    * Everything included in MiuiCamera.mk now
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,7 +186,6 @@ VENDOR_SECURITY_PATCH := 2023-05-01
 # Sepolicy
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
--include vendor/xiaomi/miuicamera/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Shims
