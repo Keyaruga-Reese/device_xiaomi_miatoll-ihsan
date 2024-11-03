c9e73461d60aa0ee9b8521b2dac3bbbe1eee461a
basamaryan <basam.aryan@gmail.com>
2024-11-03 13:58:26 +0800

    miatoll: Switch to the pixel power-libperfmgr
    [clarencelol: Drop lineage/sepolicy/libperfmgr as well and address it in DT instead]
    [clarencelol: Satisfy FCM while at it]
    
    Change-Id: Ic018a0151f7156eab2bb983e3de52cd7b8756710
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,7 +184,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
-include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
