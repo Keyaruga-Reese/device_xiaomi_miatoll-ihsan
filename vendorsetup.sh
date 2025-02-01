00370b6c010762b82c9437d74af59c149189173e
Keyaruga-Reese <keyaruga.reese@proton.me>
2025-02-01 18:58:31 +0530

    matrixx: Include Keys to Sign the build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,6 +21,9 @@ rm -rf hardware/xiaomi
 git clone https://github.com/ihsanulrahman/hardware_xiaomi -b 15 hardware/xiaomi 
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage-22.0 hardware/sony/timekeep
 
+# Keys
+echo -e "${color}Keys${end}"
+git clone https://github.com/Keyaruga-Reese/vendor_lineage-priv_keys -b matrixx vendor/lineage-priv/keys
 # Miui Camera
 #echo -e "${color}Cloning Miui Camera${end}"
 #git clone -b 15 https://github.com/ihsanulrahman/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
